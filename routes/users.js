c8ecbdedafa2964608c23d8c6d50fbb1cbbc9aae
jenniekibiri <josphatje@gmail.com>
2020-07-16 20:37:27 +0300

    change to json format

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.json('users crud');
+  res.json({ message: 'users api' });
 });
 
 module.exports = router;
